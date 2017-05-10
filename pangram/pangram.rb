5293ca5a944b87666497e1176d6b0abbbba52b97
Khoi Nguyen Trong <khoint@ITTeam.local>
2017-05-10 16:27:01 +0700

    roman-numerals solution

--- pangram/pangram.rb
+++ pangram/pangram.rb
@@ -1,5 +1,11 @@
+require 'set'
 class Pangram
-    # The quick brown fox jumps over the lazy dog
-    def self.pangram
+    ALPHABET = ('a'..'z').to_set
+    def self.pangram?(input_str)
+        ALPHABET.subset?(input_str.downcase.char.to_set)
     end
+end
+
+module BookKeeping
+  VERSION = 4
 end
